4e3ef6962f85a6c46327fa67e2993568a5e47b93
wendwin <wendwin12@gmail.com>
2025-02-18 14:14:14 +0700

    membuat formset untuk edit module

--- educa/courses/urls.py
+++ educa/courses/urls.py
@@ -22,4 +22,9 @@ urlpatterns = [
         views.CourseDeleteView.as_view(),
         name='course_delete'
     ),
+    path(
+        '<pk>/module/',
+        views.CourseModuleUpdateView.as_view(),
+        name='course_module_update'
+    ),
 ]
--- educa/courses/views.py
+++ educa/courses/views.py
@@ -6,6 +6,9 @@ from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
 from .models import Course
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic.base import TemplateResponseMixin, View
+from .forms import ModuleFormSet
 
 class OwnerMixin:
     def get_queryset(self):
@@ -48,3 +51,30 @@ class CourseDeleteView(OwnerMixin, DeleteView):
     def get_queryset(self):
         return Course.objects.filter(owner=self.request.user)
     
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/formset.html'
+    course = None
+
+    def get_formset(self, data=None):
+        return  ModuleFormSet(instance=self.course, data=data)
+    
+    def dispatch(self, request, pk):
+        self.course = get_object_or_404(
+            Course, id=pk, owner=request.user
+        )
+        return super().dispatch(request, pk)
+    
+    def get(self, request, *args, **kwargs):
+            formset = self.get_formset()
+            return self.render_to_response(
+                {'course': self.course, 'formset': formset}
+        )
+    
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response(
+            {'course': self.course, 'formset': formset}
+        )
