e094b189b05e44b7880b70b19d1c3eb9ef011ccd
wendwin <wendwin12@gmail.com>
2025-02-20 20:23:55 +0700

    menambahkan type content

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from .fields import OrderField
+from django.template.loader import render_to_string
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -66,6 +67,12 @@ class ItemBase(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def render(self):
+        return render_to_string(
+            f'courses/content/{self._meta.model_name}.html',
+            {'item': self}
+        )
     
 class Text(ItemBase):
     content = models.TextField()
